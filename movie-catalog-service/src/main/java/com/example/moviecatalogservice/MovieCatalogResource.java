3c4c3f85ad99acda3279cbf439102d06b25f10a5
Nourddine <nourddine.ait.massaoud@gmail.com>
2021-06-22 10:28:08 +0100

    commit 2

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogResource.java
@@ -1,7 +1,9 @@
 package com.example.moviecatalogservice;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,21 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        return Collections.singletonList(
-            new CatalogItem("Titanik", "test1",4)
+
+        // get all frated movies ID
+        /// for each movie ID call movie details
+        //// put them all together
+
+        List<Rating> ratings = Arrays.asList(
+            new Rating("111", 10),
+            new Rating("222", 9),
+            new Rating("333", 2),
+            new Rating("444", 6),
+            new Rating("555", 8),
+            new Rating("666", 1)
         );
+        return ratings.stream().map( rating -> (new CatalogItem("Titanik", "test1",4)
+        )).collect(Collectors.toList());
         
     }
 }
